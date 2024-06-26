0f7f21c10f068c4e77986c644af6e0ad11ee5039
Vigneshthiyagu <vignesh.thiyagarajan@neartekpod.com>
2024-06-26 14:32:21 +0530

    Contact-form

--- ntp-website-theme/tailwind.config.js
+++ ntp-website-theme/tailwind.config.js
@@ -19,14 +19,6 @@ module.exports = {
         height: "100%",
       },
 
-      // swiperslide: {
-      //   textalign: "center",
-      //   fontsize: "18px",
-      //   background: " #fff",
-      //   display: "flex",
-      //   justifycontent: "center",
-      //   alignitems: "center",
-      // },
       colors: {
         primaryblack: "#1A1A13",
         primarywhite: "#F0F0F5",
@@ -37,12 +29,20 @@ module.exports = {
         backblue: "#00A2FF",
         backyellow: "#DEF100",
         backpink: "#6100FF",
+        grassmarpisam: {
+          light: "#a8e4a0",
+          DEFAULT: "#76c7c0",
+          dark: "#4b8e8d",
+        },
       },
       fontFamily: {
         Montserrat: "Montserrat",
       },
       backgroundOpacity: {
         30: "0.3",
+      },
+      borderColor: {
+        customcolor: "linear-gradient(254deg, #FDFEFF 3.46%,  #DCEAFD 99.94%)",
       },
       backgroundImage: (theme) => ({
         WorldMap: 'url("{{get_asset_url("../../assets/Globe.svg")}}")',
@@ -55,6 +55,8 @@ module.exports = {
           'linear-gradient(to right bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.5)),url("{{get_asset_url("../../assets/bulid.png")}}")',
         Hand: 'linear-gradient(to right bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.2)),url("{{get_asset_url("../../assets/Card-image-hand-shake.svg")}}") ',
         Tech: 'linear-gradient(to right bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.2)),url("{{get_asset_url("../../assets/VR-image.svg")}}") ',
+        customgradient:
+          "linear-gradient(254deg, #FDFEFF 3.46%,  #DCEAFD 99.94%)",
       }),
     },
   },
